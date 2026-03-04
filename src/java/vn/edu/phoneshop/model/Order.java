41ed16f34e12fa22dd0afb0a8f5388e63c511012
HMSFLASH <nangd720@gmail.com>
2026-03-04 19:01:00 +0700

    Handle the order logic and connect it to UserDAO

---
+++ src/java/vn/edu/phoneshop/model/Order.java
@@ -0,0 +1,83 @@
+package vn.edu.phoneshop.model;
+
+import java.sql.Date;
+
+public class Order {
+    private int orderID;
+    private int userID;
+    private Date orderDate;
+    private double totalMoney;
+    private String shippingAddress;
+    private String note;
+    private int status;
+
+    public Order() {
+    }
+
+    public Order(int orderID, int userID, Date orderDate, double totalMoney, String shippingAddress, String note,
+            int status) {
+        this.orderID = orderID;
+        this.userID = userID;
+        this.orderDate = orderDate;
+        this.totalMoney = totalMoney;
+        this.shippingAddress = shippingAddress;
+        this.note = note;
+        this.status = status;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
